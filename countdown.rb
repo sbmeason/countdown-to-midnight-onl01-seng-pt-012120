b54233e17d0ee30e59f453c7d655b28ecc46e9a6
Samantha Meason <sbmeason@gmail.com>
2020-01-28 01:50:30 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
-
-def countdown
+def countdown(secs)
+  while secs > 0 
+  puts "#{secs} SECOND(S)!"
+  secs -= 1 
+  end
+"HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(secs)
+  while secs > 0 
+  puts "#{secs} SECOND(S)!"
+  sleep(1)
+  secs -= 1
+end
+"HAPPY NEW YEAR!"
+end
+
